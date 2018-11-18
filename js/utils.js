fd50b683f5311fa122607808809f67ab81433edb
rrramble <ru7ar7@gmail.com>
2018-11-18 17:01:54 +0600

    style: comply with linter

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,7 @@ export const makeDomNodeFromText = (text) => {
   const node = document.createElement(`div`);
   node.innerHTML = text;
   return node;
-}
+};
 
 export const isInRange = (value, min, max) => {
   return value >= min && value < max;
