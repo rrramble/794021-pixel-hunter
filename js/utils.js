358e340f900b4b86cde8097765c096d7317470af
rrramble <ru7ar7@gmail.com>
2018-11-18 15:27:20 +0600

    feature: move utility functions to appropriate module

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+export const makeDomNodeFromText = (text) => {
+  const node = document.createElement(`div`);
+  node.innerHTML = text;
+  return node;
+}
+
+export const isInRange = (value, min, max) => {
+  return value >= min && value < max;
+};
