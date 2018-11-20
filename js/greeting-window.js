ceba678bb12223d40b3ded2ac130f63837a933e4
rrramble <ru7ar7@gmail.com>
2018-11-21 03:01:05 +0600

    refactor: use class 'GameWindow' to unify window creation

--- js/greeting-window.js
+++ js/greeting-window.js
@@ -1,46 +1,40 @@
 // Экран 'Приветствие'
 
+import GameWindow from './game-window.js';
 import {changeWindow} from './utils.js';
-import rulesWindow from './rules-window.js';
+import nextWindow from './rules-window.js';
 
 const NEXT_BUTTON_SELECTOR = `.greeting__continue`;
-let nextButtonNode;
-
-const HTML_TEXT = `
-  <section class="greeting central--blur">
-    <img class="greeting__logo" src="img/logo_ph-big.svg" width="201" height="89" alt="Pixel Hunter">
-    <div class="greeting__asterisk asterisk"><span class="visually-hidden">Я просто красивая звёздочка</span>*</div>
-    <div class="greeting__challenge">
-      <h3 class="greeting__challenge-title">Лучшие художники-фотореалисты бросают тебе вызов!</h3>
-      <p class="greeting__challenge-text">Правила игры просты:</p>
-      <ul class="greeting__challenge-list">
-        <li>Нужно отличить рисунок от фотографии и сделать выбор.</li>
-        <li>Задача кажется тривиальной, но не думай, что все так просто.</li>
-        <li>Фотореализм обманчив и коварен.</li>
-        <li>Помни, главное — смотреть очень внимательно.</li>
-      </ul>
-    </div>
-    <button class="greeting__continue" type="button">
-      <span class="visually-hidden">Продолжить</span>
-      <svg class="icon" width="64" height="64" viewBox="0 0 64 64" fill="#000000">
-        <use xlink:href="img/sprite.svg#arrow-right"></use>
-      </svg>
-    </button>
-  </section>
-`;
-
-const addEventListeners = () => {
-  addNextWindowListener();
-};
-
-const addNextWindowListener = () => {
-  nextButtonNode = document.querySelector(NEXT_BUTTON_SELECTOR);
-  nextButtonNode.addEventListener(`click`, rulesWindow);
-};
+const data = {
+  innerHtml: `
+    <section class="greeting central--blur">
+      <img class="greeting__logo" src="img/logo_ph-big.svg" width="201" height="89" alt="Pixel Hunter">
+      <div class="greeting__asterisk asterisk"><span class="visually-hidden">Я просто красивая звёздочка</span>*</div>
+      <div class="greeting__challenge">
+        <h3 class="greeting__challenge-title">Лучшие художники-фотореалисты бросают тебе вызов!</h3>
+        <p class="greeting__challenge-text">Правила игры просты:</p>
+        <ul class="greeting__challenge-list">
+          <li>Нужно отличить рисунок от фотографии и сделать выбор.</li>
+          <li>Задача кажется тривиальной, но не думай, что все так просто.</li>
+          <li>Фотореализм обманчив и коварен.</li>
+          <li>Помни, главное — смотреть очень внимательно.</li>
+        </ul>
+      </div>
+      <button class="greeting__continue" type="button">
+        <span class="visually-hidden">Продолжить</span>
+        <svg class="icon" width="64" height="64" viewBox="0 0 64 64" fill="#000000">
+          <use xlink:href="img/sprite.svg#arrow-right"></use>
+        </svg>
+      </button>
+    </section>
+  `
+}
 
 const run = () => {
-  changeWindow(HTML_TEXT);
-  addEventListeners();
+  const newWindow = new GameWindow(data);
+  newWindow.pushEventListener(NEXT_BUTTON_SELECTOR, `click`, nextWindow);
+  newWindow.setRenderFunction(changeWindow);
+  return newWindow.render.bind(newWindow);
 };
 
-export default run;
+export default run();
--- js/intro-window.js
+++ js/intro-window.js
@@ -1,30 +1,24 @@
 // Экран 'Интро'
 
+import GameWindow from './game-window.js';
 import {changeWindow} from './utils.js';
 import nextWindow from './greeting-window.js';
 
 const NEXT_BUTTON_SELECTOR = `.intro__asterisk`;
-let nextButtonNode;
-
-const HTML_TEXT = `
-  <section class="intro">
-    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
-    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
-  </section>
-`;
-
-const addEventListeners = () => {
-  addNextWindowListener();
-};
-
-const addNextWindowListener = () => {
-  nextButtonNode = document.querySelector(NEXT_BUTTON_SELECTOR);
-  nextButtonNode.addEventListener(`click`, nextWindow);
+const data = {
+  innerHtml: `
+    <section class="intro">
+      <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    </section>
+  `,
 };
 
 const run = () => {
-  changeWindow(HTML_TEXT);
-  addEventListeners();
+  const newWindow = new GameWindow(data);
+  newWindow.pushEventListener(NEXT_BUTTON_SELECTOR, `click`, nextWindow);
+  newWindow.setRenderFunction(changeWindow);
+  return newWindow.render.bind(newWindow);
 };
 
-export default run;
+export default run();
