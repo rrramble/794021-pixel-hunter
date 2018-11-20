5c691b9cd846a9e40eb48e161b2b58fd36f4c194
rrramble <ru7ar7@gmail.com>
2018-11-20 20:15:43 +0600

    refactor: remove 'export' of functions not used outside the module

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,18 @@
 const MAIN_NODE = document.querySelector(`#main`);
 
-export const makeDomNodeFromText = (text) => {
+const makeDomNodeFromText = (text) => {
   const node = document.createElement(`div`);
   node.innerHTML = text;
   return node;
 };
 
-export const deleteCurrentWindow = () => {
+const deleteCurrentWindow = () => {
   for (let i = MAIN_NODE.children.length; i--;) {
     MAIN_NODE.children[i].remove();
   }
 };
 
-export const showWindow = (node) => {
+const showWindow = (node) => {
   MAIN_NODE.append(node);
 };
 
