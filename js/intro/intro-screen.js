af7940a742f7f51e7a6118bd6a15fff334064898
Rustam <ru7ar7@gmail.com>
2018-12-20 19:29:49 +0600

    feature: remove asterisk, because next screen must be accessible after downloading of images

--- js/intro/intro-screen.js
+++ js/intro/intro-screen.js
@@ -1,12 +1,10 @@
 // Presenter of the 'Intro' window
 
 import IntroView from './intro-view.js';
-import Application from '../application.js';
 
 export default class IntroScreen {
   constructor() {
     this._view = new IntroView();
-    this._view.cb = Application.showGreeting;
   }
 
   get element() {
--- js/intro/intro-view.js
+++ js/intro/intro-view.js
@@ -3,18 +3,9 @@
 import AbstractView from '../utils/abstract-view.js';
 import {makeDomNode} from '../utils.js';
 
-const EventListener = {
-  SELECTOR: `.intro__asterisk`,
-  TYPE: `click`,
-};
-
 export default class IntroView extends AbstractView {
   constructor() {
     super();
-  }
-
-  set cb(cb) {
-    this._cb = cb;
   }
 
   get element() {
@@ -22,21 +13,13 @@ export default class IntroView extends AbstractView {
   }
 
   _render() {
-    let eventListeners;
-    if (this._cb) {
-      eventListeners = [{
-        selector: EventListener.SELECTOR,
-        type: EventListener.TYPE,
-        cb: this._cb,
-      }];
-    }
-    return makeDomNode(this.template, eventListeners);
+    return makeDomNode(this.template);
   }
 
   get template() {
     return `
       <section class="intro">
-        <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+        <p class="intro__motto">Загрузка...</p>
         <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
       </section>
     `;
