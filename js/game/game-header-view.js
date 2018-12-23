27b666da0668f153a7b1bef382e689133af2e956
rrramble <ru7ar7@gmail.com>
2018-12-23 08:22:29 +0500

    fix: add blinking of 'rest seconds', instead of changing its color

--- js/game/game-header-view.js
+++ js/game/game-header-view.js
@@ -1,13 +1,12 @@
 // View class of 'Rules' window
 
 import AbstractView from '../utils/abstract-view.js';
-import {isRestSecondsOdd} from './game-utils.js';
 import {makeDomNode, makeArray} from '../utils.js';
 
 const PREVIOUS_BUTTON_SELECTOR = `.back`;
 const Blink = {
   SECONDS: 5,
-  HTML_STYLE: `style="color:red;"`,
+  CLASS: `game__timer--blink`,
 };
 
 export default class GameHeaderView extends AbstractView {
@@ -41,8 +40,25 @@ export default class GameHeaderView extends AbstractView {
     const secondsLeft = this._gameData.currentLevelSecondsLeft;
     const restLives = this._gameData.restLives;
     const lostLives = this._gameData.MAX_LIVES - restLives;
-    const blinkTemplate = isRestSecondsOdd(secondsLeft, Blink.SECONDS) ? Blink.HTML_STYLE : ``;
+    const blinkClass = secondsLeft <= Blink.SECONDS ? Blink.CLASS : ``;
     return `
+      <style>
+        .${blinkClass} {
+        animation: timer__blink 0.5s infinite;
+        animation-timing-function: steps(1, end);
+      }
+      @keyframes timer__blink {
+        from {
+          opacity: 1;
+        }
+        50% {
+          opacity: 0;
+        }
+        from {
+          opacity: 1;
+        }
+      }
+      </style>
       <header class="header">
         <button class="back">
           <span class="visually-hidden">Вернуться к началу</span>
@@ -53,7 +69,7 @@ export default class GameHeaderView extends AbstractView {
             <use xlink:href="img/sprite.svg#logo-small"></use>
           </svg>
         </button>
-        <div class="game__timer" ${blinkTemplate}>${secondsLeft}</div>
+        <div class="game__timer ${blinkClass}">${secondsLeft}</div>
         <div class="game__lives">
         ${new Array(lostLives)
           .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="31" height="27">`)
